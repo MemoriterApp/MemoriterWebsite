0a4772cccb9ae5070cf2fb029f69bf80f241e151
q-Sci <simon.hubert2904@gmail.com>
2022-10-04 22:07:11 +0200

    improved structure

--- memoriter-website-react/src/components/donate/donate-statistics.tsx
+++ memoriter-website-react/src/components/donate/donate-statistics.tsx
@@ -2,17 +2,36 @@ import React, { FC } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 
 const DonateStatistics: FC = () => {
+
+    const data: {title: string, value: number, color: string}[] = [ //data and configuration used by the pie chart
+        {
+            title: '', //data name
+            value: 35, //portion in percent
+            color: 'var(--color-highlight-gradient-blue)' //color of the data
+        },
+        {
+            title: '',
+            value: 30,
+            color: 'var(--color-container-button)'
+        },
+        {
+            title: '',
+            value: 20,
+            color: 'var(--color-container)'
+        },
+        {
+            title: '',
+            value: 15,
+            color: 'var(--color-highlight-gradient-green)'
+        }
+    ];
+
     return (
         <section>
             <PieChart
                 radius={20}
                 lineWidth={30}
-                data={[
-                    { title: 'One', value: 35, color: 'var(--color-highlight-gradient-blue)' },
-                    { title: 'Four', value: 30, color: 'var(--color-container-button)' },
-                    { title: 'Three', value: 20, color: 'var(--color-container)' },
-                    { title: 'Two', value: 15, color: 'var(--color-highlight-gradient-green)' }
-                ]}
+                data={data}
             />
         </section>
     );
