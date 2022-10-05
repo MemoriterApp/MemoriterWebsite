1abddf422daa37416469d393a30b0752b3ed01c1
q-Sci <simon.hubert2904@gmail.com>
2022-10-05 17:04:49 +0200

    final change to code blocks

--- memoriter-website-react/src/components/blog/blog-post-code-block.tsx
+++ memoriter-website-react/src/components/blog/blog-post-code-block.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import tsx from 'react-syntax-highlighter/dist/esm/languages/prism/tsx';
-import coldarkDark from 'react-syntax-highlighter/dist/esm/styles/prism/coldark-dark';
-import coldarkCold from 'react-syntax-highlighter/dist/esm/styles/prism/coldark-cold';
+import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
+import dracula from 'react-syntax-highlighter/dist/cjs/styles/prism/dracula';
+import ghcolors from 'react-syntax-highlighter/dist/cjs/styles/prism/ghcolors';
 
 SyntaxHighlighter.registerLanguage('tsx', tsx); //adding programming language for syntax highlighting
 
@@ -13,10 +13,20 @@ interface props { //type definitions of props
 
 const BlogPostCodeBlock: FC<props> = ({ children }: props) => {
 
-    const codeBlockStyles: React. CSSProperties = { //modifications to pre built syntax highlighter (from a library)
+    const codeBlockStylesDark: React.CSSProperties = { //modifications to pre built syntax highlighter (from a library), used for the dark theme
         fontSize: '1.2rem',
         lineHeight: '2rem',
         padding: '12px 16px 12px 16px',
+        marginBottom: '20px',
+        backgroundColor: 'var(--color-hover)',
+        borderRadius: '10px'
+    };
+
+    const codeBlockStylesLight: React.CSSProperties = { //modifications to pre built syntax highlighter (from a library), used for the light theme
+        fontSize: '1.325rem',
+        lineHeight: '2rem',
+        padding: '12px 16px 12px 16px',
+        marginBottom: '20px',
         backgroundColor: 'var(--color-hover)',
         borderRadius: '10px'
     };
@@ -25,10 +35,10 @@ const BlogPostCodeBlock: FC<props> = ({ children }: props) => {
 
     return ( //a library for syntax highlighting is used
         <>
-            {theme === 'dark' && <SyntaxHighlighter language='tsx' style={coldarkDark} customStyle={codeBlockStyles} wrapLongLines={true}>
+            {theme === 'dark' && <SyntaxHighlighter language='tsx' style={dracula} customStyle={codeBlockStylesDark} wrapLongLines={true}>
                 {children.join('\n')} {/*children refers to the content inside the wrapper, .join('\n) creates line breaks, the content needs to be an array*/}
             </SyntaxHighlighter>}
-            {theme === 'light' && <SyntaxHighlighter language='tsx' style={coldarkCold} customStyle={codeBlockStyles} wrapLongLines={true}>
+            {theme === 'light' && <SyntaxHighlighter language='tsx' style={ghcolors} customStyle={codeBlockStylesLight} wrapLongLines={true}>
                 {children.join('\n')} {/*children refers to the content inside the wrapper, .join('\n) creates line breaks, the content needs to be an array*/}
             </SyntaxHighlighter>}
         </>
