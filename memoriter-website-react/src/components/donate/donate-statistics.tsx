59b907245d62341a6ac789e50c14880c2f0588e7
q-Sci <simon.hubert2904@gmail.com>
2022-10-22 16:11:24 +0200

    dynamic label

--- memoriter-website-react/src/components/donate/donate-statistics.tsx
+++ memoriter-website-react/src/components/donate/donate-statistics.tsx
@@ -13,33 +13,54 @@ const DonateStatistics: FC = () => {
         transition: 'opacity 400ms'
     };
 
-    const data: {title: string, value: number, color: string}[] = [ //data and configuration used by the pie chart
+    const data: {value: number, color: string}[] = [ //data and configuration used by the pie chart
         {
-            title: '', //data name
             value: 35, //amount of money
             color: 'var(--color-highlight-gradient-blue)' //color of the data
         },
         {
-            title: '',
             value: 30,
             color: 'var(--color-container-button)'
         },
         {
-            title: '',
             value: 20,
             color: 'var(--color-container)'
         },
         {
-            title: '',
             value: 15,
             color: 'var(--color-highlight-gradient-green)'
         }
     ];
 
-    const [onHover, setOnHover] = useState<number | null>(null); //state if a section of the chart is hovered
+    const dataSum = data.reduce((index: number, items: {value: number, color: string}) => index + items.value, 0); //sum of all data values
+
+    const [onHover, setOnHover] = useState<number | null>(null); //state if a section of the chart is hovered over
+    const [label, setLabel] = useState<React.ReactNode>( //state to set the current label
+        <><h3>{dataSum}€</h3><p>Total Spending</p></>
+    );
+    const [contentSet, setContentSet] = useState<boolean>(true); //state to check if the content is set to prevent an infite loop
+
+    //conditional statement to set the label based on the section hovered over
+    if (onHover === 0 && ! contentSet) {
+        setLabel(<><h3 style={{color : data[0].color}}>{data[0].value}€</h3><p>Spending</p></>);
+        setContentSet(true);
+    } else if (onHover === 1 && ! contentSet) {
+        setLabel(<><h3 style={{color : data[1].color}}>{data[1].value}€</h3><p>Spending</p></>);
+        setContentSet(true);
+    } else if (onHover === 2 && ! contentSet) {
+        setLabel(<><h3 style={{color : data[2].color}}>{data[2].value}€</h3><p>Spending</p></>);
+        setContentSet(true);
+    } else if (onHover === 3 && ! contentSet) {
+        setLabel(<><h3 style={{color : data[3].color}}>{data[3].value}€</h3><p>Spending</p></>);
+        setContentSet(true);
+    } else if (!contentSet) {
+        setLabel(<><h3>{dataSum}€</h3><p>Total Spending</p></>);
+        setContentSet(true);
+    };
 
     return (
-        <section>
+        <section className='donate-statistics'>
+            {/*interactive pie chart*/}
             <PieChart
                 className='donate-statistics-chart'
                 radius={50}
@@ -51,11 +72,27 @@ const DonateStatistics: FC = () => {
                 data={data}
                 onMouseOver={(_: React.MouseEvent, index: number) => {
                     setOnHover(index);
+                    setContentSet(false);
                 }}
                 onMouseOut={() => {
                     setOnHover(null);
+                    setTimeout(() => {setContentSet(false)}, 50); //setTimeout is used to fix the short change between the gaps 
                 }}
             />
+            {/*chart label*/}
+            <article className='donate-statistics-chart-label'>
+                {label}
+            </article>
+
+            {/*content next to the chart*/}
+            <article>
+                <h2>LOL</h2>
+                <p>LOL</p>
+            </article>
+            <article>
+                <h2>LOL</h2>
+                <p>LOL</p>
+            </article>
         </section>
     );
 }
