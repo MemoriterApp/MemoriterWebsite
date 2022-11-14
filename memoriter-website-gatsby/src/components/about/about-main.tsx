bc61d7d72d8ca1cfa6f76a74685c26c857b23e5b
q-Sci <simon.hubert2904@gmail.com>
2022-11-14 21:56:18 +0100

    full about page scss modules

--- memoriter-website-gatsby/src/components/about/about-main.tsx
+++ memoriter-website-gatsby/src/components/about/about-main.tsx
@@ -1,70 +1,70 @@
 import React, { FC } from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-import '../../styles/about/about-main.scss';
+import * as styles from '../../styles/about/about-main.module.scss';
 import placeholderImage from '../../images/about/placeholder.jpg';
 
 const AboutMain: FC = () => {
     return (
-        <section className='about-main'> {/*main parts with texts and images*/}
+        <section className={styles.about_main}> {/*main parts with texts and images*/}
 
-            <article className='about-main-container' style={{marginTop: '160px'}}>
-                <div className='about-main-container-sub'>
-                    <AnimationOnScroll animateIn='about-main-animation-fade-in-right' animateOut='about-main-animation-fade-out-right'>
+            <article className={styles.about_main_container} style={{marginTop: '160px'}}>
+                <div className={styles.about_main_container_sub}>
+                    <AnimationOnScroll animateIn={styles.about_main_animation_fade_in_right} animateOut={styles.about_main_animation_fade_out_right}>
                     {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-                        <h1 className='about-main-heading'>Who we are.</h1>
-                        <p className='about-main-text'>
+                        <h1 className={styles.about_main_heading}>Who we are.</h1>
+                        <p className={styles.about_main_text}>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                         </p>
                     </AnimationOnScroll>
                 </div>
-                <div className='about-main-container-sub-image'>
-                    <img className='about-main-image' src={placeholderImage} alt='placeholder'/>
+                <div className={styles.about_main_container_sub_image}>
+                    <img className={styles.about_main_image} src={placeholderImage} alt='placeholder'/>
                 </div>
             </article>
-            <article className='about-main-container'>
-                <div className='about-main-container-sub'>
-                    <AnimationOnScroll animateIn='about-main-animation-fade-in-right' animateOut='about-main-animation-fade-out-right'>
+            <article className={styles.about_main_container}>
+                <div className={styles.about_main_container_sub}>
+                    <AnimationOnScroll animateIn={styles.about_main_animation_fade_in_right} animateOut={styles.about_main_animation_fade_out_right}>
                     {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-                        <p className='about-main-text'>
+                        <p className={styles.about_main_text}>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                         </p>
                     </AnimationOnScroll>
                 </div>
-                <div className='about-main-container-sub-image'>
-                    <img className='about-main-image' src={placeholderImage} alt='placeholder'/>
-                </div>
-            </article>
-
-            <article className='about-main-container' style={{marginTop: '240px'}}>
-                <div className='about-main-container-sub-image'>
-                    <img className='about-main-image' src={placeholderImage} alt='placeholder'/>
-                </div>
-                <div className='about-main-container-sub'>
-                    <AnimationOnScroll animateIn='about-main-animation-fade-in-left' animateOut='about-main-animation-fade-out-left'>
-                    {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-                        <h1 className='about-main-heading'>Placeholder Heading</h1>
-                        <p className='about-main-text'>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                        </p>
-                    </AnimationOnScroll>
-                </div>
-            </article>
-            <article className='about-main-container'>
-                <div className='about-main-container-sub-image'>
-                    <img className='about-main-image' src={placeholderImage} alt='placeholder'/>
-                </div>
-                <div className='about-main-container-sub'>
-                    <AnimationOnScroll animateIn='about-main-animation-fade-in-left' animateOut='about-main-animation-fade-out-left'>
-                    {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-                        <p className='about-main-text'>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                        </p>
-                    </AnimationOnScroll>
+                <div className={styles.about_main_container_sub_image}>
+                    <img className={styles.about_main_image} src={placeholderImage} alt='placeholder'/>
                 </div>
             </article>
 
-            <AnimationOnScroll animateIn='about-main-animation-fade-in-down' animateOut='about-main-animation-fade-out-down'>
-                <p className='about-main-bottom'>
+            <article className={styles.about_main_container} style={{marginTop: '240px'}}>
+                <div className={styles.about_main_container_sub}>
+                    <img className={styles.about_main_image} src={placeholderImage} alt='placeholder'/>
+                </div>
+                <div className={styles.about_main_container_sub}>
+                    <AnimationOnScroll animateIn={styles.about_main_animation_fade_in_left} animateOut={styles.about_main_animation_fade_out_left}>
+                    {/*AnimationOnScroll starts an animation when it enters the viewport*/}
+                        <h1 className={styles.about_main_heading}>Placeholder Heading</h1>
+                        <p className={styles.about_main_text}>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        </p>
+                    </AnimationOnScroll>
+                </div>
+            </article>
+            <article className={styles.about_main_container}>
+                <div className={styles.about_main_container_sub}>
+                    <img className={styles.about_main_image} src={placeholderImage} alt='placeholder'/>
+                </div>
+                <div className={styles.about_main_container_sub}>
+                    <AnimationOnScroll animateIn={styles.about_main_animation_fade_in_left} animateOut={styles.about_main_animation_fade_out_left}>
+                    {/*AnimationOnScroll starts an animation when it enters the viewport*/}
+                        <p className={styles.about_main_text}>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        </p>
+                    </AnimationOnScroll>
+                </div>
+            </article>
+
+            <AnimationOnScroll animateIn={styles.about_main_animation_fade_in_down} animateOut={styles.about_main_animation_fade_out_down}>
+                <p className={styles.about_main_bottom}>
                     Lorem <span>ipsum</span> dolor sit amet, consetetur <span>sadipscing</span> elitr.
                 </p>
             </AnimationOnScroll>
