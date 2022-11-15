f08091c1fc8d500123b82bc20a341af82c5c5e53
q-Sci <simon.hubert2904@gmail.com>
2022-11-15 12:51:14 +0100

    small design change

--- memoriter-website-gatsby/src/components/product/product-banner.tsx
+++ memoriter-website-gatsby/src/components/product/product-banner.tsx
@@ -40,8 +40,12 @@ const ProductBanner: FC = () => {
             {/*content*/}
             <article className='product-banner-content'>
                 {/*slogan*/}
-                <p className='product-banner-text'>Study better faster smarter - study with Memoriter</p>
-                <p className='product-main-overview-text' style={{width:'100%', marginBottom:'75px'}}>Work with Memoriter effective memorizing, tools to quickly achieve successful results. </p>
+                <p className='product-banner-text'>
+                    Study Better, Faster, Smarter - Study with Memoriter!
+                </p>
+                <p className='product-banner-text-sub'>
+                    Work with Memoriter effective memorizing, tools to quickly achieve successful results.
+                </p>
 
                 {/*get started button for redirecting to register page*/}
                 <a
