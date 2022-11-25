2f2aaeee65ca33e3863997ea42e1f1424416793e
q-Sci <simon.hubert2904@gmail.com>
2022-11-25 18:57:05 +0100

    more modules

--- memoriter-website-gatsby/src/components/download/download-main.tsx
+++ memoriter-website-gatsby/src/components/download/download-main.tsx
@@ -1,83 +1,105 @@
 import React, { FC, useState } from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-import '../../styles/download/download-main.scss';
+import * as styles from '../../styles/download/download-main.module.scss';
 import windowsIcon from '../../images/icons/windows-icon.svg';
 import appleIcon from '../../images/icons/apple-icon.svg';
 import placeholderImage from '../../images/download/placeholder.jpg';
 
 const DownloadMain: FC = () => {
-    
-    const [onHoverWindows, setOnHoverWindows] = useState<string>('brightness(1)'); //variable for the hover effect for the download for windows button
-    const [onHoverMac, setOnHoverMac] = useState<string>('brightness(1)'); //variable for the hover effect for the download for mac button
-    
-    return (
-        <section className='download-main'>
 
-            <h1 className='download-main-title'>Download Memoriter</h1>
-            <p className='download-main-top-text'>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+  const [onHoverWindows, setOnHoverWindows] = useState<string>('brightness(1)'); //variable for the hover effect for the download for windows button
+  const [onHoverMac, setOnHoverMac] = useState<string>('brightness(1)'); //variable for the hover effect for the download for mac button
+
+  return (
+    <section className={styles.download_main}>
+      <h1 className={styles.download_main_title}>Download Memoriter</h1>
+      <p className={styles.download_main_top_text}>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      </p>
+      <img className={styles.download_main_top_image} src={placeholderImage} alt='placeholder' />
+
+      <article className={styles.download_main_container} style={{ marginTop: '300px' }}>
+        <div className={styles.download_main_container_sub_image}>
+          <img className={styles.download_main_image} src={placeholderImage} alt='placeholder' />
+        </div>
+        <div className={styles.download_main_container_sub}>
+          <AnimationOnScroll
+            animateIn={styles.download_main_animation_fade_in_left}
+            animateOut={styles.download_main_animation_fade_out_left}
+          >
+            {/*AnimationOnScroll starts an animation when it enters the viewport*/}
+            <h1 className={styles.download_main_heading}>Placeholder Heading</h1>
+            <p className={styles.download_main_text}>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
             </p>
-            <img className='download-main-top-image' src={placeholderImage} alt='placeholder'/>
+          </AnimationOnScroll>
+        </div>
+      </article>
 
-            <article className='download-main-container' style={{marginTop: '300px'}}>
-                <div className='download-main-container-sub-image'>
-                    <img className='download-main-image' src={placeholderImage} alt='placeholder'/>
-                </div>
-                <div className='download-main-container-sub'>
-                    <AnimationOnScroll animateIn='download-main-animation-fade-in-left' animateOut='download-main-animation-fade-out-left'>
-                    {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-                        <h1 className='download-main-heading'>Placeholder Heading</h1>
-                        <p className='download-main-text'>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                        </p>
-                    </AnimationOnScroll>
-                </div>
-            </article>
+      <article className={styles.download_main_container}>
+        <div className={styles.download_main_container_sub}>
+          <AnimationOnScroll
+            animateIn={styles.download_main_animation_fade_in_right}
+            animateOut={styles.download_main_animation_fade_out_right}
+          >
+            {/*AnimationOnScroll starts an animation when it enters the viewport*/}
+            <h1 className={styles.download_main_heading}>Placeholder Heading</h1>
+            <p className={styles.download_main_text}>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            </p>
+          </AnimationOnScroll>
+        </div>
+        <div className={styles.download_main_container_sub_image}>
+          <img className={styles.download_main_image} src={placeholderImage} alt='placeholder' />
+        </div>
+      </article>
 
-            <article className='download-main-container'>
-                <div className='download-main-container-sub'>
-                    <AnimationOnScroll animateIn='download-main-animation-fade-in-right' animateOut='download-main-animation-fade-out-right'>
-                    {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-                        <h1 className='download-main-heading'>Placeholder Heading</h1>
-                        <p className='download-main-text'>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                        </p>
-                    </AnimationOnScroll>
-                </div>
-                <div className='download-main-container-sub-image'>
-                    <img className='download-main-image' src={placeholderImage} alt='placeholder'/>
-                </div>
-            </article>
+      <article className={styles.download_main_footer}>
+        <p className={styles.download_main_footer_text}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+        </p>
+        {/*download button for windows*/}
+        <button
+          className={`${styles.download_main_download} ${styles.download_main_download_windows}`} //two classes
+          onMouseEnter={() => setOnHoverWindows('brightness(0.75)')}
+          onMouseLeave={() => setOnHoverWindows('brightness(1)')}
+        >
+          {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css and the background animation*/}
+          <div className={styles.download_main_download_background} style={{ filter: onHoverWindows }} />
+          <img
+            className={styles.download_main_download_icon}
+            src={windowsIcon}
+            alt='windows-icon'
+            style={{ height: '36px' }}
+          />
+          <span className={styles.download_main_download_text}>Download</span>
+          <span className={styles.download_main_download_text_os}>for Windows</span>
+        </button>
 
-            <article className='download-main-footer'>
-                <p className='download-main-footer-text'>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-                </p>
-                {/*download button for windows*/}
-                <button className='download-main-download download-main-download-windows' //two classes
-                    onMouseEnter={() => setOnHoverWindows('brightness(0.75)')} onMouseLeave={() => setOnHoverWindows('brightness(1)')}>
-                    {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css and the background animation*/}
-                    <div className='download-main-download-background' style={{filter: onHoverWindows}}/>
-                    <img className='download-main-download-icon' src={windowsIcon} alt='windows-icon' style={{height: '36px'}}/>
-                    <span className='download-main-download-text'>Download</span>
-                    <span className='download-main-download-text-os'>for Windows</span>
-                </button>
+        {/*download button for mac*/}
+        <button
+          className={`${styles.download_main_download} ${styles.download_main_download_mac}`} //two classes
+          onMouseEnter={() => setOnHoverMac('brightness(0.75)')}
+          onMouseLeave={() => setOnHoverMac('brightness(1)')}
+        >
+          {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css and the background animation*/}
+          <div className={styles.download_main_download_background} style={{ filter: onHoverMac }} />
+          <img
+            className={styles.download_main_download_icon}
+            src={appleIcon}
+            alt='mac-icon'
+            style={{ height: '42px' }}
+          />
+          <span className={styles.download_main_download_text}>Download</span>
+          <span className={styles.download_main_download_text_os}>for Mac</span>
+        </button>
+      </article>
 
-                {/*download button for mac*/}
-                <button className='download-main-download download-main-download-mac' //two classes
-                    onMouseEnter={() => setOnHoverMac('brightness(0.75)')} onMouseLeave={() => setOnHoverMac('brightness(1)')}>
-                    {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css and the background animation*/}
-                    <div className='download-main-download-background' style={{filter: onHoverMac}}/>
-                    <img className='download-main-download-icon' src={appleIcon} alt='mac-icon' style={{height: '42px'}}/>
-                    <span className='download-main-download-text'>Download</span>
-                    <span className='download-main-download-text-os'>for Mac</span>
-                </button>
-            </article>
-
-            <hr className='download-main-divider'/>
-
-        </section>
-    );
-}
-
+      <hr className={styles.download_main_divider} />
+    </section>
+  );
+};
 export default DownloadMain;
--- memoriter-website-gatsby/src/components/download/download-mobile.tsx
+++ memoriter-website-gatsby/src/components/download/download-mobile.tsx
@@ -1,56 +1,55 @@
 import React, { FC } from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-import '../../styles/download/download-mobile.scss';
+import * as styles from '../../styles/download/download-mobile.module.scss';
 import phone from '../../images/download/phone.png';
 import phoneContent from '../../images/download/phone-content.jpg';
 import googlePlayButton from '../../images/download/google-play-button.svg';
 import appStoreButton from '../../images/download/app-store-button.svg';
 
 const DownloadMobile: FC = () => {
-    return (
-        <section className='download-mobile'>
-
-            <h1 className='download-mobile-heading-alt'>Mobile App</h1> {/*alternative section heading for small screens*/}
-
-            {/*mobile phone image with content*/}
-            <div className='download-mobile-phone-image'>
-                <img className='download-mobile-phone-image-outside' src={phone} alt='mobile-phone'/>
-                <img className='download-mobile-phone-image-content' src={phoneContent} alt='mobile-phone-content'/>
-            </div>
-
-            {/*content text*/}
-            <AnimationOnScroll animateIn='download-mobile-animation-fade-in-down' animateOut='download-mobile-animation-fade-out-down'>
-                <article className='download-mobile-content'>
-
-                    <h2 className='download-mobile-heading'>Mobile App</h2>
-
-                    <h3 className='download-mobile-content-heading'>Experience the efficiency and flexibility of Memoriter &#8212; to go</h3> {/*&#8212; is a unicode dash symbol*/}
-                    <div className='download-mobile-content-text'>
-                        <p>
-                            The Memoriter mobile app brings all web and desktop features to the phone.
-                        </p>
-                        <ul>
-                            <li>Phone and tablet support</li>
-                            <li>Data synced to web and desktop app</li>
-                            <li>Notes, study mode and all other tools you need</li>
-                        </ul>
-                    </div>
-                        
-                    {/*play store and app store links*/}
-                    <div className='download-mobile-buttons'>
-                        <a href='https://play.google.com/store/' target='_blank' rel='noreferrer'>
-                            <img src={googlePlayButton} alt='google-play-button'/>
-                        </a>
-                        <a href='https://www.apple.com/app-store/' target='_blank' rel='noreferrer'>
-                            <img src={appStoreButton} alt='app-store-button'/>
-                        </a>
-                    </div>
-
-                </article>
-            </AnimationOnScroll>
-
-        </section>
-    );
-}
-
+  return (
+    <section className={styles.download_mobile}>
+      <h1 className={styles.download_mobile_heading_alt}>Mobile App</h1> {/*alternative section heading for small screens*/}
+      
+      {/*mobile phone image with content*/}
+      <div className={styles.download_mobile_phone_image}>
+        <img className={styles.download_mobile_phone_image_outside} src={phone} alt='mobile-phone' />
+        <img
+          className={styles.download_mobile_phone_image_content}
+          src={phoneContent}
+          alt='mobile-phone-content'
+        />
+      </div>
+      {/*content text*/}
+      <AnimationOnScroll
+        animateIn={styles.download_mobile_animation_fade_in_down}
+        animateOut={styles.download_mobile_animation_fade_out_down}
+      >
+        <article className={styles.download_mobile_content}>
+          <h2 className={styles.download_mobile_heading}>Mobile App</h2>
+          <h3 className={styles.download_mobile_content_heading}>
+            Experience the efficiency and flexibility of Memoriter &#8212; to go
+          </h3> {/*&#8212; is a unicode dash symbol*/}
+          <div className={styles.download_mobile_content_text}>
+            <p>The Memoriter mobile app brings all web and desktop features to the phone.</p>
+            <ul>
+              <li>Phone and tablet support</li>
+              <li>Data synced to web and desktop app</li>
+              <li>Notes, study mode and all other tools you need</li>
+            </ul>
+          </div>
+          {/*play store and app store links*/}
+          <div className={styles.download_mobile_buttons}>
+            <a href='https://play.google.com/store/' target='_blank' rel='noreferrer'>
+              <img src={googlePlayButton} alt='google-play-button' />
+            </a>
+            <a href='https://www.apple.com/app-store/' target='_blank' rel='noreferrer'>
+              <img src={appStoreButton} alt='app-store-button' />
+            </a>
+          </div>
+        </article>
+      </AnimationOnScroll>
+    </section>
+  );
+};
 export default DownloadMobile;
