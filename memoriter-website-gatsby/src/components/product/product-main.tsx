dce5cb46cc93477856dfc38423badf2ef963b5e9
q-Sci <simon.hubert2904@gmail.com>
2022-11-30 21:38:47 +0100

    even more scss modules

--- memoriter-website-gatsby/src/components/product/product-main.tsx
+++ memoriter-website-gatsby/src/components/product/product-main.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import { StaticImage } from 'gatsby-plugin-image';
-import '../../styles/product/product-main.scss';
+import * as styles from '../../styles/product/product-main.module.scss';
 import lightBulbIconGradient from '../../images/product/light-bulb.svg';
 import handShakeIcon from '../../images/product/handshake.svg';
 import remember from '../../images/product/remember.svg';
@@ -9,83 +9,83 @@ import free from '../../images/product/free.svg';
 
 const ProductMain: FC = () => {
   return (
-    <section className='product-main'>
+    <section className={styles.product_main}>
       {/*three small overview items*/}
-      <section className='product-main-overview'>
+      <section className={styles.product_main_overview}>
         <article>
           <img
-            className='product-main-overview-image'
+            className={styles.product_main_overview_image}
             src={lightBulbIconGradient}
-            alt='light-bulb'
+            alt='Light bulb'
           />
-          <p className='product-main-overview-text'>
+          <p className={styles.product_main_overview_text}>
             Accumulate knowledge faster with flashcards
           </p>
         </article>
 
         <article>
-          <img className='product-main-overview-image' src={handShakeIcon} alt='placeholder' />
-          <p className='product-main-overview-text'>
+          <img className={styles.product_main_overview_image} src={handShakeIcon} alt='placeholder' />
+          <p className={styles.product_main_overview_text}>
             Study with friends and share your knowledge
           </p>
         </article>
 
         <article>
-          <img className='product-main-overview-image' src={remember} alt='placeholder' />
-          <p className='product-main-overview-text'>
+          <img className={styles.product_main_overview_image} src={remember} alt='placeholder' />
+          <p className={styles.product_main_overview_text}>
             Remeber everything with spaced repetition
           </p>
         </article>
 
         <article>
-          <img className='product-main-overview-image' src={free} alt='placeholder' />
-          <p className='product-main-overview-text'>
+          <img className={styles.product_main_overview_image} src={free} alt='placeholder' />
+          <p className={styles.product_main_overview_text}>
             The best part: It's only free.99
           </p>
         </article>
       </section>
 
       {/*main parts with texts and images*/}
-      <article className='product-main-container'>
-        <div className='product-main-container-sub'>
+      <article className={styles.product_main_container}>
+        <div className={styles.product_main_container_sub}>
           <AnimationOnScroll
-            animateIn='product-main-animation-fade-in-right'
-            animateOut='product-main-animation-fade-out-right'
+            animateIn={styles.product_main_animation_fade_in_right}
+            animateOut={styles.product_main_animation_fade_out_right}
           >
             {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-            <h1 className='product-main-heading'>Accumalate knowledge faster</h1>
-            <p className='product-main-text'>
+            <h1 className={styles.product_main_heading}>Accumalate knowledge faster</h1>
+            <p className={styles.product_main_text}>
             Flashcards have proven to be <strong >150% more effective</strong> if you want to accumalte knowledge faster. 
             Instead of memorizing your notes passively, save time using flashcards. When studying with flashcards 
             the algorithm will notice your mistakes and help you study more where your knowledge is lacking!
             </p>
           </AnimationOnScroll>
         </div>
-        <div className='product-main-container-sub-image'>
+        <div className={styles.product_main_container_sub_image}>
           <StaticImage
-            className='product-main-image'
+            className={styles.product_main_image}
             src='../../images/product/placeholder.jpg'
             alt='placeholder'
           />
         </div>
       </article>
 
-      <article className='product-main-container'>
-        <div className='product-main-container-sub-image'>
+      <article className={styles.product_main_container}>
+        <div className={styles.product_main_container_sub_image}>
           <StaticImage
-            className='product-main-image'
+            className={styles.product_main_image}
             src='../../images/product/placeholder.jpg'
             alt='placeholder'
           />
         </div>
-        <div className='product-main-container-sub'>
+        <div className={styles.product_main_container_sub}>
           <AnimationOnScroll
-            animateIn='product-main-animation-fade-in-left'
-            animateOut='product-main-animation-fade-out-left'
+            animateIn={styles.product_main_animation_fade_in_left}
+            animateOut={styles.product_main_animation_fade_out_left}
           >
             {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-            <h1 className='product-main-heading'>Study with friends</h1>
-            <p className='product-main-text'>
+            <h1 className={styles.product_main_heading}>Study with friends</h1>
+            <p className={styles.product_main_text}>
             Study together and you will always find the motivation to suceed. 
             You can help each other out and share your notes, techniques and structure. 
             That way you will save time and hard work.
@@ -94,44 +94,44 @@ const ProductMain: FC = () => {
         </div>
       </article>
 
-      <article className='product-main-container'>
-        <div className='product-main-container-sub'>
+      <article className={styles.product_main_container}>
+        <div className={styles.product_main_container_sub}>
           <AnimationOnScroll
-            animateIn='product-main-animation-fade-in-right'
-            animateOut='product-main-animation-fade-out-right'
+            animateIn={styles.product_main_animation_fade_in_right}
+            animateOut={styles.product_main_animation_fade_out_right}
           >
             {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-            <h1 className='product-main-heading'>Remember everything with spaced repetition</h1>
-            <p className='product-main-text'>
+            <h1 className={styles.product_main_heading}>Remember everything with spaced repetition</h1>
+            <p className={styles.product_main_text}>
               By using the scientic method of spaced repetition, we will remind you to regularly review important flashcards 
               that way you can remember thousands of flashcards with minimal effort.
             </p>
           </AnimationOnScroll>
         </div>
-        <div className='product-main-container-sub-image'>
+        <div className={styles.product_main_container_sub_image}>
           <StaticImage
-            className='product-main-image'
+            className={styles.product_main_image}
             src='../../images/product/placeholder.jpg'
             alt='placeholder'
           />
         </div>
       </article>
 
-      <article className='product-main-container'>
-        <div className='product-main-container-sub-image'>
+      <article className={styles.product_main_container}>
+        <div className={styles.product_main_container_sub_image}>
           <StaticImage
-            className='product-main-image'
+            className={styles.product_main_image}
             src='../../images/product/placeholder.jpg'
             alt='placeholder'
           />
         </div>
-        <div className='product-main-container-sub'>
+        <div className={styles.product_main_container_sub}>
           <AnimationOnScroll
-            animateIn='product-main-animation-fade-in-left'
-            animateOut='product-main-animation-fade-out-left'
+            animateIn={styles.product_main_animation_fade_in_left}
+            animateOut={styles.product_main_animation_fade_out_left}
           >
-            <h1 className='product-main-heading'>It's only free.99 </h1>
-            <p className='product-main-text'>
+            <h1 className={styles.product_main_heading}>It's only free.99 </h1>
+            <p className={styles.product_main_text}>
               Food is always beetter when it's free, same goes for software. We strictly believe in the idea that education
               and the things around it should be made affordable. That's why we decided to make Memoriter free.99.
             </p>
--- memoriter-website-gatsby/src/components/product/product-stories.tsx
+++ memoriter-website-gatsby/src/components/product/product-stories.tsx
@@ -1,98 +1,124 @@
 import React, { FC, useState } from 'react';
-import '../../styles/product/product-stories.scss';
+import * as styles from '../../styles/product/product-stories.module.scss';
 
 const ProductStories: FC = () => {
 
-    const stories: {number: number, author: string, quote: string}[] = [ //array with all story data
-        {
-            number: 0,
-            author: 'Harry',
-            quote: 'I used to struggle a lot with latin in school. Every week our Teacher would give us a huge list of words to study. I would spend hours memorizing them, but when using memoriter I was able to learn them in a fraction of the time, thanks to the different learning methods.'
-        },
-        {
-            number: 1,
-            author: 'Hermoine',
-            quote: 'When studying in med. school it is imporant to remember everything over a longer period of time. Memoriter helped me to learn the most important things for my exams. I was able to learn everything in a short amount of time and to remember it for a long time by using spaced repetition.'
-        },
-        {
-            number: 2,
-            author: 'Ron',
-            quote: 'I often times studied in groups. Memoriter helped us to study together and to share our knowledge. We were able to create our own flashcards and to study them together. This way we were able to learn everything faster and to remember it for a longer time.'
-        },
-    ];
+  const stories: { number: number; author: string; quote: string }[] = [ // array with all story data
+    {
+      number: 0,
+      author: 'Harry',
+      quote:
+        'I used to struggle a lot with latin in school. Every week our Teacher would give us a huge list of words to study. I would spend hours memorizing them, but when using memoriter I was able to learn them in a fraction of the time, thanks to the different learning methods.',
+    },
+    {
+      number: 1,
+      author: 'Hermoine',
+      quote:
+        'When studying in med. school it is imporant to remember everything over a longer period of time. Memoriter helped me to learn the most important things for my exams. I was able to learn everything in a short amount of time and to remember it for a long time by using spaced repetition.',
+    },
+    {
+      number: 2,
+      author: 'Ron',
+      quote:
+        'I often times studied in groups. Memoriter helped us to study together and to share our knowledge. We were able to create our own flashcards and to study them together. This way we were able to learn everything faster and to remember it for a longer time.',
+    },
+  ];
 
-    const [number, setNumber] = useState<number>(Math.floor(Math.random() * stories.length)); //number of the currently shown story (position in the array), is random on page render
+  const [number, setNumber] = useState<number>(Math.floor(Math.random() * stories.length)); // number of the currently shown story (position in the array), is random on page render
 
-    const [textAreaAnimation, setTextAreaAnimation] = useState<string>('1'); //value used for the animation on next or previous story
-    const [dotAnimation, setDotAnimation] = useState<string>('var(--color-highlight-gradient-green)');
+  const [textAreaAnimation, setTextAreaAnimation] = useState<string>('1'); // value used for the animation on next or previous story
+  const [dotAnimation, setDotAnimation] = useState<string>('var(--color-highlight-gradient-green)');
 
-    const previousStory = (): void => { //function for switching to previous story
-        setTextAreaAnimation('0'); //fade out effect
-        setTimeout(() => {setTextAreaAnimation('1')}, 400); //fade in effect
+  const previousStory = (): void => { // function for switching to previous story
+    setTextAreaAnimation('0'); // fade out effect
+    setTimeout(() => {
+      setTextAreaAnimation('1');
+    }, 400); // fade in effect
 
-        setDotAnimation('var(--color-hover)'); //dot fade out
-        setTimeout(() => {setDotAnimation('var(--color-highlight-gradient-green)')}, 400); //dot fade in
-        
-        setTimeout(() => { //timeout needed for correctly executed animation
-            if (number === 0) { //if the first story is shown it switches to the last one
-                setNumber(stories.length - 1);
-            } else { //else it switches to the previous one
-                setNumber(number - 1);
-            };
-        }, 400);
-    };
+    setDotAnimation('var(--color-hover)'); // dot fade out
+    setTimeout(() => {
+      setDotAnimation('var(--color-highlight-gradient-green)');
+    }, 400); // dot fade in
 
-    const nextStory = (): void => { //function for switching to next story
-        setTextAreaAnimation('0'); //fade out effect
-        setTimeout(() => {setTextAreaAnimation('1');}, 400); //fade in effect
+    setTimeout(() => { // timeout needed for correctly executed animation
+      if (number === 0) { // if the first story is shown it switches to the last one
+        setNumber(stories.length - 1);
+      } else { // else it switches to the previous one
+        setNumber(number - 1);
+      }
+    }, 400);
+  };
 
-        setDotAnimation('var(--color-hover)'); //dot fade out
-        setTimeout(() => {setDotAnimation('var(--color-highlight-gradient-green)')}, 400); //dot fade in
-        
-        setTimeout(() => { //timeout needed for correctly executed animation
-            if (number === stories.length - 1) { //if the first story is shown it switches to the last one
-                setNumber(0);
-            } else { //else it switches to the previous one
-                setNumber(number + 1);
-            };
-        }, 400);
-    };
+  const nextStory = (): void => { // function for switching to next story
+    setTextAreaAnimation('0'); // fade out effect
+    setTimeout(() => {
+      setTextAreaAnimation('1');
+    }, 400); // fade in effect
 
-    const chosenStory = (story: {number: number}): void => { //function for switching to the story of the clicked dot
-        setTextAreaAnimation('0'); //fade out effect
-        setTimeout(() => {setTextAreaAnimation('1');}, 400); //fade in effect
+    setDotAnimation('var(--color-hover)'); // dot fade out
+    setTimeout(() => {
+      setDotAnimation('var(--color-highlight-gradient-green)');
+    }, 400); // dot fade in
 
-        setDotAnimation('var(--color-hover)'); //dot fade out
-        setTimeout(() => {setDotAnimation('var(--color-highlight-gradient-green)')}, 400); //dot fade in
-        
-        setTimeout(() => {setNumber(story.number)}, 400); //timeout needed for correctly executed animation
-    };
+    setTimeout(() => { // timeout needed for correctly executed animation
+      if (number === stories.length - 1) { // if the first story is shown it switches to the last one
+        setNumber(0);
+      } else { // else it switches to the previous one
+        setNumber(number + 1);
+      }
+    }, 400);
+  };
 
-    return (
-        <section className='product-stories'>
-            <div className='product-stories-textarea'> {/*the values from the array are used here*/}
-                <article style={{opacity: textAreaAnimation, transition: 'opacity 400ms'}}> {/*style used for animation*/}
-                    <p className='product-stories-textarea-quote'>{stories[number].quote}</p>
-                    <p className='product-stories-textarea-author'>- {stories[number].author}</p>  
-                </article>
-            </div>
+  const chosenStory = (story: { number: number }): void => { // function for switching to the story of the clicked dot
+    setTextAreaAnimation('0'); // fade out effect
+    setTimeout(() => {
+      setTextAreaAnimation('1');
+    }, 400); // fade in effect
 
-            {/*buttons for switching to next or previous*/}
-            <button className='product-stories-arrow-left' onClick={previousStory}/>
-            <button className='product-stories-arrow-right' onClick={nextStory}/>
+    setDotAnimation('var(--color-hover)'); // dot fade out
+    setTimeout(() => {
+      setDotAnimation('var(--color-highlight-gradient-green)');
+    }, 400); // dot fade in
 
-            {/*the bar of dots at the bottom for selecting a story*/}
-            <div className='product-stories-dots'>
-                {stories.map((story) => (
-                    story.number === number? ( //dot for the current story is highlighted
-                        <div className='product-stories-dot' key={story.number} style={{background: dotAnimation}}/>
-                    ) : (
-                        <div className='product-stories-dot' key={story.number} onClick={() => chosenStory(story)}/>
-                    )
-                ))}
-            </div>
-        </section>
-    );
+    setTimeout(() => {
+      setNumber(story.number);
+    }, 400); // timeout needed for correctly executed animation
+  };
+
+  return (
+    <section className={styles.product_stories}>
+      <div className={styles.product_stories_textarea}>
+        {/*the values from the array are used here*/}
+        <article style={{ opacity: textAreaAnimation, transition: 'opacity 400ms' }}>
+          {/*style used for animation*/}
+          <p className={styles.product_stories_textarea_quote}>{stories[number].quote}</p>
+          <p className={styles.product_stories_textarea_author}>- {stories[number].author}</p>
+        </article>
+      </div>
+
+      {/*buttons for switching to next or previous*/}
+      <button className={styles.product_stories_arrow_left} onClick={previousStory} />
+      <button className={styles.product_stories_arrow_right} onClick={nextStory} />
+
+      {/*the bar of dots at the bottom for selecting a story*/}
+      <div className={styles.product_stories_dots}>
+        {stories.map((story) =>
+          story.number === number ? ( // dot for the current story is highlighted
+            <div
+              className={styles.product_stories_dot}
+              key={story.number}
+              style={{ background: dotAnimation }}
+            />
+          ) : (
+            <div
+              className={styles.product_stories_dot}
+              key={story.number}
+              onClick={() => chosenStory(story)}
+            />
+          )
+        )}
+      </div>
+    </section>
+  );
 };
-
 export default ProductStories;
