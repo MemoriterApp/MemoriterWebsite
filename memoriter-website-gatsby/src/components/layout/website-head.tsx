50397118884928206c254618f01c7702c56ecf9b
q-Sci <simon.hubert2904@gmail.com>
2022-12-03 15:45:12 +0100

    small changes

--- memoriter-website-gatsby/src/components/layout/website-head.tsx
+++ memoriter-website-gatsby/src/components/layout/website-head.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-interface Props { //type definitions for props
+interface Props { // type definitions for props
   type: 'website' | 'article';
   title: string;
   description: string;
@@ -15,17 +15,16 @@ const WebsiteHead: FC<Props> = ({ title, description, keywords, type }: Props) =
       <meta name='author' content='Memoriter'/>
       <meta name='description' content={description}/>
       <meta name='keywords' content={`memoriter, learning, study, notes, school, spaced repetition, ${keywords}`}/>
-      <meta name="theme-color" content="#202020"/>
+      <meta name='theme-color' content='#202020'/>
       {/*facebook tags*/}
-      <meta property="og:title" content={`Memoriter | ${title}`}/>
-      <meta property="og:description" content={description}/>
-      <meta property="og:type" content={type}/>
+      <meta property='og:title' content={`Memoriter | ${title}`}/>
+      <meta property='og:description' content={description}/>
+      <meta property='og:type' content={type}/>
       {/*twitter tags*/}
-      <meta name="twitter:title" content={`Memoriter | ${title}`}/>
-      <meta name="twitter:description" content={description}/>
-      <meta name="twitter:card" content='summary'/>
+      <meta name='twitter:title' content={`Memoriter | ${title}`}/>
+      <meta name='twitter:description' content={description}/>
+      <meta name='twitter:card' content='summary'/>
     </>
   );
 };
-
 export default WebsiteHead;
