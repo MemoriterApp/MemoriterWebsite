2977a38c1c7bd85cfd97d1a68a0299afcd7202ab
q-Sci <simon.hubert2904@gmail.com>
2023-01-23 21:12:25 +0100

    optimized stylesheet

--- src/components/bot/bot-main-footer.tsx
+++ src/components/bot/bot-main-footer.tsx
@@ -1,29 +1,29 @@
 import React, { FC, useState } from 'react';
-import * as styles from '../../styles/default-main.module.scss';
+import * as styles from '../../styles/button-footer.module.scss';
 import discordIcon from '../../images/icons/discord-icon.svg';
 
 const BotMainFooter: FC = () => {
   const [onHover, setOnHover] = useState<string>('brightness(1)'); //variable for the hover effect for the add to discord button
 
   return (
-    <article className={styles.default_main_download_footer}>
-        <p className={styles.default_main_download_footer_text}>
+    <article className={styles.button_footer}>
+        <p className={styles.button_footer_text}>
           Integrate the CheatTP-3 in your existing workflow.
         </p>
         {/*download button for windows*/}
         <button
-          className={styles.default_main_bot}
+          className={styles.button_footer_bot}
           onMouseEnter={() => setOnHover('brightness(0.75)')}
           onMouseLeave={() => setOnHover('brightness(1)')}
         >
           {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css and the background animation*/}
-          <div className={styles.default_main_bot_background} style={{ filter: onHover }} />
+          <div className={styles.button_footer_bot_background} style={{ filter: onHover }} />
           <img
-            className={styles.default_main_bot_icon}
+            className={styles.button_footer_bot_icon}
             src={discordIcon}
             alt='Discord icon'
           />
-          <span className={styles.default_main_bot_text}>Add to Discord</span>
+          <span className={styles.button_footer_bot_text}>Add to Discord</span>
         </button>
       </article>
   );
--- src/components/download/download-main-footer.tsx
+++ src/components/download/download-main-footer.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from 'react';
-import * as styles from '../../styles/default-main.module.scss';
+import * as styles from '../../styles/button-footer.module.scss';
 import windowsIcon from '../../images/icons/windows-icon.svg';
 import appleIcon from '../../images/icons/apple-icon.svg';
 
@@ -9,51 +9,51 @@ const DownloadMainFooter: FC = () => {
 
   return (
     <>
-      <article className={styles.default_main_download_footer}>
-        <p className={styles.default_main_download_footer_text}>
+      <article className={styles.button_footer}>
+        <p className={styles.button_footer_text}>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
         </p>
         {/*download button for windows*/}
         <button
-          className={`${styles.default_main_download} ${styles.default_main_download_windows}`} //two classes
+          className={`${styles.button_footer_download} ${styles.button_footer_download_windows}`} //two classes
           onMouseEnter={() => setOnHoverWindows('brightness(0.75)')}
           onMouseLeave={() => setOnHoverWindows('brightness(1)')}
         >
           {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css and the background animation*/}
           <div
-            className={styles.default_main_download_background}
+            className={styles.button_footer_download_background}
             style={{ filter: onHoverWindows }}
           />
           <img
-            className={styles.default_main_download_icon}
+            className={styles.button_footer_download_icon}
             src={windowsIcon}
             alt='windows-icon'
             style={{ height: '36px' }}
           />
-          <span className={styles.default_main_download_text}>Download</span>
-          <span className={styles.default_main_download_text_os}>for Windows</span>
+          <span className={styles.button_footer_download_text}>Download</span>
+          <span className={styles.button_footer_download_text_os}>for Windows</span>
         </button>
 
         {/*download button for mac*/}
         <button
-          className={`${styles.default_main_download} ${styles.default_main_download_mac}`} //two classes
+          className={`${styles.button_footer_download} ${styles.button_footer_download_mac}`} //two classes
           onMouseEnter={() => setOnHoverMac('brightness(0.75)')}
           onMouseLeave={() => setOnHoverMac('brightness(1)')}
         >
           {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css and the background animation*/}
-          <div className={styles.default_main_download_background} style={{ filter: onHoverMac }} />
+          <div className={styles.button_footer_download_background} style={{ filter: onHoverMac }} />
           <img
-            className={styles.default_main_download_icon}
+            className={styles.button_footer_download_icon}
             src={appleIcon}
             alt='mac-icon'
             style={{ height: '42px' }}
           />
-          <span className={styles.default_main_download_text}>Download</span>
-          <span className={styles.default_main_download_text_os}>for Mac</span>
+          <span className={styles.button_footer_download_text}>Download</span>
+          <span className={styles.button_footer_download_text_os}>for Mac</span>
         </button>
       </article>
 
-      <hr className={styles.default_main_download_divider} />
+      <hr className={styles.button_footer_divider} />
     </>
   );
 };
