eab9cd4255afab99c7a3546d93042205c27685ee
q-Sci <simon.hubert2904@gmail.com>
2023-02-18 22:01:18 +0100

    imroved impressum

--- src/pages/impressum.tsx
+++ src/pages/impressum.tsx
@@ -29,6 +29,27 @@ const Impressum: FC = () => {
           <li>Email: johan@memoriter.de</li>
         </ul>
 
+        <h2>Responsibility for Content</h2>
+        <p>
+          As a service provider we are legally responsible for our own content. However, we are not
+          required to supervise transferred or stored external information or to search for
+          circumstances that might hint at illegal activities.
+        </p>
+        <p>
+          Obligation to remove or block access to information according to general law are
+          unaffected by this. Legal responsibility for this is only possible after we have been
+          informed of concrete law violations, then we can remove these contents immediately..
+        </p>
+
+        <h2>Responsibility for Links</h2>
+        <p>
+          This site contains links to third-party websites whose content is not controlled by us.
+          Therefore we cannot take responsibility for their content. The publishers of external
+          websites are legally responsible for their content. No violations were recognizable at the
+          time of linking. We will remove affected links immediately when notified of law
+          violations.
+        </p>
+
         <h2>Copyright</h2>
         <p>
           As a student and community driven project, we decided to distribute Memoriter as an open
