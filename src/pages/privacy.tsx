dd502767cf4e6f253261bd5d7cb7e58e9188786b
q-Sci <simon.hubert2904@gmail.com>
2023-02-22 21:46:29 +0100

    way to long ToU/ToS

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -404,14 +404,14 @@ const Privacy: FC = () => {
         </p>
 
         <h2>10. Changes to this Privacy Policy</h2>
-          <p>
-            We may update this privacy notice from time to time. The updated version will be
-            indicated by an updated date and the updated version will be effective as soon
-            as it is accessible. If we make material changes to this privacy notice, we may notify
-            you either by prominently posting a notice of such changes or by directly sending you a
-            notification. We encourage you to review this privacy notice frequently to be informed
-            of how we are protecting your information.
-          </p>
+        <p>
+          We may update this privacy policy from time to time. The updated version will be indicated
+          by an updated date and the updated version will be effective as soon as it is accessible.
+          If we make material changes to this privacy policy, we may notify you either by
+          prominently posting a notice of such changes or by directly sending you a notification. We
+          encourage you to review this privacy policy frequently to be informed of how we are
+          protecting your information.
+        </p>
       </article>
     </WebsiteLayout>
   );
